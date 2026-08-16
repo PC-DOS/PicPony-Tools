46d0807142eec2e6540e328e021afcbff71bfa72
PC-DOS <suicunexp@outlook.com>
2026-08-16 23:32:20 +0800

    Added scripted trans

---
+++ TranslationProgress/ScriptedTrans.py
@@ -0,0 +1,67 @@
+import os
+import pickle
+import datetime
+import pathlib
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import TranslatedTagsUtil as Trans
+from DerpibooruDatabaseDump import DerpibooruDatabaseDump
+
+# Dump an object to persistent file on disk
+def DumpObjectToFile(objInstance : object, sPath : str) :
+    import pickle
+    with open(sPath, "wb") as filObjDump :
+        pickle.dump(objInstance, filObjDump)
+    #End With
+#End Sub
+
+# Load an object from persistent file on disk
+def LoadObjectFromFile(sPath : str) -> object :
+    import pickle
+    with open(sPath, "rb") as filObjDump :
+        objInstance = pickle.load(filObjDump)
+    #End With
+    return objInstance
+#End Function
+
+if __name__ == "__main__" :
+    # Load translations
+    sTransFile = "tags_translated_1786807445574.txt"
+    dctTrans = Trans.LoadTranslatedTags(sTransFile)
+    sTransTimestamp = pathlib.Path(sTransFile).name
+    sTransTimestamp = sTransTimestamp.removeprefix("tags_translated_").removesuffix(".txt")
+    dUnixTimestampMs = int(sTransTimestamp)
+    dtmTransTimestamp = datetime.datetime.fromtimestamp(dUnixTimestampMs / 1000.0)
+    sTransTimestamp = dtmTransTimestamp.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Translation file timestamp: {sTransTimestamp}")
+    
+    # Load source trans
+    sTragetFile = "tags_page_1.txt"
+    sProcessedFile = "tags_trans_output.txt"
+    dctTarget = Trans.LoadTranslatedTags(sTragetFile)
+    
+    # Batch translating
+    nTotal = 0
+    nProcessed = 0
+    nSkipped = 0
+    for CurrentTag in dctTarget.keys() :
+        sBaseTag = CurrentTag.removeprefix("fusion:")
+        if sBaseTag in dctTrans.keys() :
+            arrCurrentSource = dctTrans[sBaseTag]["TransCn"]
+            dctTarget[CurrentTag]["TransCn"] = []
+            for CurrentSource in arrCurrentSource :
+                dctTarget[CurrentTag]["TransCn"].append(f"融合：{CurrentSource}")
+            #Next
+            nProcessed += 1
+        else :
+            nSkipped += 1
+        #End If
+        nTotal += 1
+    #Next
+    print(f"Total: {nTotal}, Processed: {nProcessed}, Skipped: {nSkipped}")
+    
+    # Outputting
+    Trans.ExportTranslatedTags(dctTarget, sProcessedFile)
+#End If
--- TranslationProgress/TranslatedTagsUtil.py
+++ TranslationProgress/TranslatedTagsUtil.py
@@ -28,9 +28,9 @@ def LoadTranslatedTags(sFile : str) -> dict :
         for CurrentTrans in arrTraslationLines :
             arrTagSeparated = SplitString(CurrentTrans, " - B:")
             arrTransSeparated = SplitString(arrTagSeparated[1], " - C:")
-            sTag = arrTagSeparated[0].removeprefix("A:").removeprefix("\"").removesuffix("\"")
-            sTrans = arrTransSeparated[0].removeprefix("\"").removesuffix("\"")
-            sDesc = arrTransSeparated[1].removeprefix("\"").removesuffix("\"")
+            sTag = arrTagSeparated[0].removeprefix("A:")
+            sTrans = arrTransSeparated[0]
+            sDesc = arrTransSeparated[1]
             
             dctCurrentTrans = dict(TransCn=[], Desc="")
             dctCurrentTrans["TransCn"] = SplitString(sTrans, ",")
@@ -43,3 +43,31 @@ def LoadTranslatedTags(sFile : str) -> dict :
     
     return dctTags
 #End Function
+
+# Export translated tags
+def ExportTranslatedTags(dctTags : dict, sFile : str) :
+    nProcessdTags = 0
+    
+    with open(sFile, "w", encoding="utf-8") as filTranslation :
+        print(f"Writing translation to file {sFile}")
+        
+        arrTags = list(dctTags.keys())
+        nTagCount = len(arrTags)
+        print(f"{nTagCount} tag translation entries found")
+        
+        for i in range(0, nTagCount) :
+            sTag = arrTags[i]
+            sTrans = ",".join(dctTags[sTag]["TransCn"])
+            sDesc = dctTags[sTag]["Desc"]
+            sCurrentLine = f"A:{sTag} - B:{sTrans} - C:{sDesc}"
+            if i < nTagCount - 1 :
+                sCurrentLine = sCurrentLine + " // "
+            #End If
+            filTranslation.write(sCurrentLine)
+            nProcessdTags += 1
+        #Next
+        
+        print(f"{nProcessdTags} tag(s) processed")
+    #End With
+#End Sub
+    
