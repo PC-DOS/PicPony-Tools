02d85054e01dd0c20181c71e49bd177cebd8029c
PC-DOS <suicunexp@outlook.com>
2026-08-17 19:40:26 +0800

    Update ScriptedTrans.py

--- TranslationProgress/ScriptedTrans.py
+++ TranslationProgress/ScriptedTrans.py
@@ -27,6 +27,38 @@ def LoadObjectFromFile(sPath : str) -> object :
     return objInstance
 #End Function
 
+# Intersect arrays (get intersection)
+# Duplicate elements will be merged
+# If sEmptyListOperation is set to "none", will do normal mathematical intersection calculation (Set1 & EmptySet == EmptySet)
+# If sEmptyListOperation is set to "keep" and one of the given arrays is empty, intersection will not be calculated, and will return another array (Set1 & EmptySet == Set1)
+def IntersectArrays(arrArray1 : list, arrArray2 : list,
+    IsDuplicateItemsDropped : bool = True, sEmptyListOperation : str = "none") -> list :
+
+    # Check if one of the given arrays is empty
+    if sEmptyListOperation.lower() == "keep" :
+        if len(arrArray1) == 0 :
+            return copy.deepcopy(arrArray2)
+        elif len(arrArray2) == 0 :
+            return copy.deepcopy(arrArray1)
+        #End If
+    #End If
+
+    arrResult = []
+    for CurrentElement in arrArray1 :
+        if (CurrentElement in arrResult) and IsDuplicateItemsDropped :
+            continue
+        #End If
+        if CurrentElement in arrArray2 :
+            if (CurrentElement in arrResult) and IsDuplicateItemsDropped :
+                continue
+            #End If
+            arrResult.append(CurrentElement)
+        #End If
+    #Next
+
+    return arrResult
+#End Function
+
 if __name__ == "__main__" :
     # Load translations
     sTransFile = "tags_translated_1786945553220.jsonl"
@@ -42,6 +74,32 @@ if __name__ == "__main__" :
     sTragetFile = "tags_page_1.jsonl"
     sProcessedFile = "tags_trans_output.jsonl"
     dctTarget = Trans.LoadTranslatedTags(sTragetFile)
+    
+    # Load Derpibooru database dump
+    IsDerpibooruDbNeeded = False 
+    if IsDerpibooruDbNeeded :
+        dbDerpibooru = DerpibooruDatabaseDump("derpibooru_public_dump_2026_08_15.pgdump")
+        dbDerpibooru.PrintInfo()
+        sDerpibooruTimestamp = dbDerpibooru.GetTimestamp()
+        print("Getting tags ...")
+        dctTagsByName = dbDerpibooru.GetTagsByName()
+        dctTagsById = dbDerpibooru.GetTagsById()
+        nTags = len(dctTagsById.keys())
+        print(f"{nTags} tags found in database dump")
+        print("Getting image tags ...")
+        if os.path.exists("_Cache/dctImageTags.pkl") :
+            with open("_Cache/dctImageTags.pkl", "rb") as filObjDump : 
+                dctImageTags = pickle.load(filObjDump)
+            #End With
+        else :
+            dctImageTags = dbDerpibooru.GetImageTags()
+            with open("_Cache/dctImageTags.pkl", "wb") as filObjDump : 
+                pickle.dump(dctImageTags, filObjDump)
+            #End With
+        #End If
+        nImages = len(dctImageTags.keys())
+        print(f"{nImages} images found in database dump")
+    #End If
     
     # Batch translating
     nTotal = 0
@@ -212,6 +270,62 @@ if __name__ == "__main__" :
                 dctTarget[CurrentTag]["TransCn"] = [f"性感蠢萌{sBaseTag}"]
                 nProcessed += 1
             #End If
+        elif CurrentTag.startswith("ship:") :
+            try :
+                # Find shipping couple tags by indexing images
+                print(f"    Ship tag: {CurrentTag}")
+                iCurrentShipTag = dctTagsByName[CurrentTag]["Id"]
+                arrTagIntersection = []
+                for CurrentImg in dctImageTags.keys() :
+                    if CurrentImg in dctImageHides.keys() :
+                        continue
+                    #End If
+                    arrTagIds = dctImageTags[CurrentImg]
+                    arrTagIntersection = IntersectArrays(arrTagIntersection, arrTagIds, sEmptyListOperation="keep")
+                #Next
+                print(f"        Intersected tag IDs: {arrTagIntersection}")
+                
+                # Map tag intersection to text
+                arrTagIntersectionStr = []
+                for CurrentId in arrTagIntersection :
+                    if dctTagsById[CurrentId]["Category"] is None :
+                        continue
+                    elif dctTagsById[CurrentId]["Category"].lower() == "character" :
+                        arrTagIntersectionStr.append(dctTagsById[CurrentId]["Name"])
+                    elif dctTagsById[CurrentId]["Name"].startswith("oc:") :
+                        arrTagIntersectionStr.append(dctTagsById[CurrentId]["Name"])
+                    #End If
+                #Next
+                print(f"        Intersected tag names: {arrTagIntersectionStr}")
+                
+                # Translating
+                sResult = "cp:"
+                for i in range(0, len(arrTagIntersectionStr)) :
+                    if arrTagIntersectionStr[i].startswith("oc:") :
+                        if arrTagIntersectionStr[i] in dctTrans.keys() :
+                            sResult = sResult + dctTrans[arrTagIntersectionStr[i]]["TransCn"][0].removeprefix("oc:")
+                        else :
+                            sResult = sResult + arrTagIntersectionStr[i].removeprefix("oc:") + "（OC）"
+                        #End If
+                    else :
+                        if arrTagIntersectionStr[i] in dctTrans.keys() :
+                            sResult = sResult + dctTrans[arrTagIntersectionStr[i]]["TransCn"][0]
+                        else :
+                            sResult = sResult + arrTagIntersectionStr[i]
+                        #End If
+                    #End If
+                    if i < len(arrTagIntersectionStr)-1 :
+                        sResult = sResult + "x"
+                    #End If
+                #Next
+                print(f"        Proposed result: {sResult}")
+                
+                # Outputting
+                dctTarget[CurrentTag]["TransCn"] = [f"{sResult}"]
+                nProcessed += 1
+            except :
+                nSkipped += 1
+            #End Try
         else :
             nSkipped += 1
         #End If
